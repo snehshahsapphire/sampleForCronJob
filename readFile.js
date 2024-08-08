2ed03b1cc2e6987897dbf90ba4a0d323b971629b
snehshahsapphire <sneh.shah.sapphire@gmail.com>
2024-08-08 10:20:04 +0530

    Add files via upload

---
+++ readFile.js
@@ -0,0 +1,8 @@
+// Function to log the current time to the console
+function logCurrentTime() {
+    const now = new Date();
+    console.log(`Current time: ${now.toLocaleTimeString()}`);
+}
+
+// Call the function to log the current time
+logCurrentTime();
